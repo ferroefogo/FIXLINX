6618aeeb439c57a6ec86dda93e85698a7041a54c
u2190045 <marco.fernandes@warwick.ac.uk>
2024-01-11 13:46:30 +0000

    rework 2.0

--- cogs/linkScanner/cog.py
+++ cogs/linkScanner/cog.py
@@ -4,25 +4,23 @@ import re
 
 xfix = "https://fxtwitter.com/"
 twtfix = "https://fixupx.com/"
-url_regex = r'(https?://[^\s]+)'
 
 class LinkScannerCog(commands.Cog, name="Link Scanner"):
     def __init__(self, bot: commands.Bot):
         self.__bot = bot
 
-    def fix_url(self, match):
-        url = match.group(1)
-        if "twitter.com" in url:
-            return twtfix + url.replace("https://twitter.com/", "")
-        elif "x.com" in url:
-            return xfix + url.replace("http://x.com/", "")
-
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         if not message.author.bot:
             # Avoid infinite loop.
-            fixed_string = re.sub(url_regex, self.fix_url, message.content)
-            await message.channel.send(fixed_string)
+            if "fxtwitter.com/" not in message.content and "fixupx.com/" not in message.content:
+                # Avoid already fixed links.
+                if "twitter.com/" in message.content:
+                    new_msg = message.content.replace("twitter.com/", "fxtwitter.com/")
+                    await message.channel.send(new_msg)
+                elif "x.com/" in message.content:
+                    new_msg = message.content.replace("x.com/", "fixupx.com/")
+                    await message.channel.send(new_msg)
 
 def setup(bot):
     bot.add_cog(LinkScannerCog(bot))
