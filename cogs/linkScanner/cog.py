2789a7f7777070600355f58e5850eca57943ff13
u2190045 <marco.fernandes@warwick.ac.uk>
2024-01-11 13:26:05 +0000

    alg rework

--- cogs/linkScanner/cog.py
+++ cogs/linkScanner/cog.py
@@ -1,31 +1,28 @@
 import nextcord
 from nextcord.ext import commands
+import re
 
-formats_fixes = {
-    "twitter.com": "fx",
-    "x.com": "fixup"
-}
+xfix = "https://fxtwitter.com/"
+twtfix = "https://fixupx.com/"
+url_regex = r'(https?://[^\s]+)'
 
 class LinkScannerCog(commands.Cog, name="Link Scanner"):
     def __init__(self, bot: commands.Bot):
         self.__bot = bot
 
+    def fix_url(self, match):
+        url = match.group(1)
+        if "twitter.com" in url:
+            return twtfix + url.replace("https://twitter.com/", "")
+        elif "x.com" in url:
+            return xfix + url.replace("http://x.com/", "")
 
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         if not message.author.bot:
             # Avoid infinite loop.
-            # Check link format against fixes
-            for format, fix in formats_fixes.items():
-                msg_start_index = message.content.find(format)
-                if msg_start_index != -1:
-                    if format == "x.com":
-                        new_content = "https://" + fix + message.content[msg_start_index:]
-                        await message.channel.send(new_content)
-                    elif format == "twitter.com":
-                        new_content = "https://" + fix + message.content[msg_start_index:]
-                        await message.channel.send(new_content)
-
+            fixed_string = re.sub(url_regex, self.fix_url, message.content)
+            await message.channel.send(fixed_string)
 
 def setup(bot):
     bot.add_cog(LinkScannerCog(bot))
