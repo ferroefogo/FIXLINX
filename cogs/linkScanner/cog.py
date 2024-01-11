f436d8a16738c60db5b148ac74c95997cf6311d1
u2190045 <marco.fernandes@warwick.ac.uk>
2024-01-11 11:17:16 +0000

    Bot sends message instead of user (not currently possible)

--- cogs/linkScanner/cog.py
+++ cogs/linkScanner/cog.py
@@ -13,16 +13,18 @@ class LinkScannerCog(commands.Cog, name="Link Scanner"):
 
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
-        # Check link format against fixes
-        for format, fix in formats_fixes.items():
-            msg_start_index = message.content.find(format)
-            if msg_start_index != -1:
-                if format == "x.com":
-                    new_content = "https://" + fix + message.content[msg_start_index:]
-                elif format == "twitter.com":
-                    new_content = "https://" + fix + message.content[msg_start_index:]
-        # Known twt link formats: www.twitter.com/..., www.x.com/...
-        message.edit(new_content)
+        if not message.author.bot:
+            # Avoid infinite loop.
+            # Check link format against fixes
+            for format, fix in formats_fixes.items():
+                msg_start_index = message.content.find(format)
+                if msg_start_index != -1:
+                    if format == "x.com":
+                        new_content = "https://" + fix + message.content[msg_start_index:]
+                        await message.channel.send(new_content)
+                    elif format == "twitter.com":
+                        new_content = "https://" + fix + message.content[msg_start_index:]
+                        await message.channel.send(new_content)
 
 
 def setup(bot):
